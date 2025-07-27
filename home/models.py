f020f233e4d2635c5d1f97f8a142cbfc9ec3201f
Erfan <erfanshadkam@outlook.com>
2025-07-27 21:39:01 +0330

    2nd commit

--- home/models.py
+++ home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class home(models.Model):
+    name = models.CharField(max_length=100)
+    job = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='images')
+    description = models.TextField()
+    
+    def __str__(self):
+        return self.name
--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from . import views
+from home import views
+from Portfo_Website import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('', views.home, name='home'),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
